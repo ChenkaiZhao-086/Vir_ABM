d46399607ffc2d788527243bc160cd7a03104d12
Chenkai_Zhao <ck_zhao@163.com>
2023-10-25 21:35:53 +0800

    First cpp code, HOORAY ^_^

--- Code/ModelSimCpp.cpp
+++ Code/ModelSimCpp.cpp
@@ -11,10 +11,10 @@ using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
 
 // 计算每一列累计病例数
-rowvec ComputeInfeCase(const mat &agent_status)
+vec ComputeInfeCase(const mat &agent_status)
 {
-    umat binaryMatrix = (agent_status == 1);            // Binary matrix with 1s where agent_status is 1
-    return conv_to<rowvec>::from(sum(binaryMatrix, 0)); // Sum each column and convert
+    umat binaryMatrix = (agent_status == 1);         // Binary matrix with 1s where agent_status is 1
+    return conv_to<vec>::from(sum(binaryMatrix, 0)); // Sum each column and convert, 0 is stand for column
     /*
     conv_to<T>::from是Armadillo提供的一个模板函数，用于将数据从一种类型转换为另一种类型。
     在这里，它将上面的求和结果转换（或强制转换）为rowvec类型（即行向量类型）。
@@ -22,115 +22,8 @@ rowvec ComputeInfeCase(const mat &agent_status)
     */
 }
 
-// 计算状态转换概率
-mat ComputeStateTrans(int time, float dt, float beta_seasonal, float beta_phi, size_t N, float gamma,
-                      const mat &agent_status, rowvec infe_cases, rowvec beta_value,
-                      rowvec comp_value, rowvec omega_value)
-{
-    mat state_probability(N, 8, fill::zeros);
-    float seasonal_force = (1 + beta_seasonal * cos(2 * M_PI * time / 52) - beta_phi);
-    float gamma_prob = 1 - exp(-gamma * dt);
-
-    for (size_t i = 0; i < N; i++)
-    {
-        for (size_t j = 0; j < 8; j++)
-        {
-            switch ((int)agent_status(i, j))
-            {
-            case 0:
-            {
-                float beta = beta_value(j) * seasonal_force;
-                // pow是元素级别的求指数，与R中的(comp_j / comp_k)^inf_states功能一致，prod是连乘，与R一致
-                vec inf_states = conv_to<vec>::from(agent_status.row(i) == 1);
-                float delta = prod(pow((comp_value(j) / comp_value), inf_states));
-                float lambda = beta * delta * infe_cases(j) / N;
-                state_probability(i, j) = 1 - exp(-lambda * dt);
-            }
-            break;
-
-            case 1:
-                state_probability(i, j) = gamma_prob;
-                break;
-            case -1:
-                state_probability(i, j) = 1 - exp(-omega_value(j) * dt);
-                break;
-            }
-            /*
-             if (agent_status(i, j) == 0)
-                        {
-                            float beta = beta_value(j) * seasonal_force;
-                            float delta = 1;
-                            float comp_j = comp_value(j);
-                            // pow是元素级别的求指数，与R中的(comp_j / comp_k)^inf_states功能一致，prod是连乘，与R一致
-                            vec inf_states = conv_to<vec>::from(agent_status.row(i) == 1);
-                            delta = prod(pow((comp_j / comp_value), inf_states));
-
-                            float lambda = beta * delta * infe_cases(j) / N;
-                            state_probability(i, j) = 1 - exp(-lambda * dt);
-                        }
-                        else if (agent_status(i, j) == 1)
-                        {
-                            state_probability(i, j) = gamma_prob;
-                        }
-                        else if (agent_status(i, j) == -1)
-                        {
-                            state_probability(i, j) = 1 - exp(-omega_value(j) * dt);
-                        }
-            */
-        }
-    }
-
-    return state_probability;
-}
-
-// 状态转换
-mat stateTransition(mat &State_transition, mat &agent_status)
-{
-    // Create the random matrix and comparing matrix
-    umat Comparing_matrix = (State_transition > randu<mat>(State_transition.n_rows, State_transition.n_cols));
-
-    /*
-        mat agent_status2 = agent_status;
-        //在mat中使用等号复制一个矩阵会产生一个新的副本，不会对原始的agent_status产生影响
-        //似乎这里不需要复制一个agent_status2了，因为下面的三个condition已经将要修改的位置限定死了
-    */
-
-    // State transition
-    umat condition_S = (agent_status == 0) % Comparing_matrix;
-    umat condition_I = (agent_status == 1) % Comparing_matrix;
-    umat condition_R = (agent_status == -1) % Comparing_matrix;
-
-    agent_status.elem(find(condition_S)).fill(1);
-    agent_status.elem(find(condition_I)).fill(-1);
-    agent_status.elem(find(condition_R)).fill(0);
-
-    return agent_status;
-}
-
-// 增加新的易感者
-mat AddCases(mat agent_status, int added_cases)
-{
-    int num_cols = agent_status.n_cols;
-
-    for (size_t col = 0; col < num_cols; col++)
-    {
-        uvec zero_indicater = find(agent_status.col(col) == 0); // zero_indicater是一个标记了哪里是0的向量，如agent_status中第1，3，5个为0，那么zero_indicater则返回1，3，5
-
-        if (zero_indicater.n_elem >= added_cases)
-        {
-            zero_indicater = shuffle(zero_indicater); // 这里将zero_indicater的顺序打乱，这样在下一步找到10个填入病例的时候才能实现随机
-            for (size_t k = 0; k < added_cases; k++)
-            {
-                agent_status(zero_indicater(k), col) = 1;
-            }
-        }
-    }
-
-    return agent_status;
-}
-
 // [[Rcpp::export]]
-mat ModelSimCpp(List Parm)
+mat ModelSimCpp(List Parm, int ncores)
 {
     size_t years = Parm["years"];
     float dt = Parm["dt"];
@@ -153,70 +46,43 @@ mat ModelSimCpp(List Parm)
     vec omega_value = {Parm["omega_IFVA"], Parm["omega_IFVB"], Parm["omega_RSV"], Parm["omega_HPIV"],
                        Parm["omega_HMPV"], Parm["omega_HCoV"], Parm["omega_HRV"], Parm["omega_HAdV"]};
 
-    mat agent_status(N, 8, arma::fill::zeros);
-    // Put initial seeds
-    agent_status.rows(0, initial_seeds - 1).fill(1);
-
-    // agent_status2 = copy(agent_status);
-
-    // Create state transition probability matrix
-    mat State_probability(N, 8, fill::zeros);
-
-    // Create comparing matrix
-    mat Comparing_matrix(N, 8, fill::zeros);
-
-    // Create storage for simulation results
-    mat results(steps + 1, 9, fill::zeros);              // 使用Armadillo创建一个零矩阵，mat创建的是双精度浮点数，umat创建的是无符号整数
+    // Create a series of matrix
+    // 使用Armadillo创建一个零矩阵，mat创建的是双精度浮点数，umat创建的是无符号整数
+    mat agent_status(N, 8, arma::fill::zeros);           // Store status for each agent
+    agent_status.rows(0, initial_seeds - 1).fill(1);     // Put initial infectious seeds
+    mat State_probability(N, 8, fill::zeros);            // Create state transition probability matrix
+    mat Comparing_matrix(N, 8, fill::zeros);             // Create comparing matrix
+    mat results(steps + 1, 9, fill::zeros);              // Create storage for simulation results
     results.col(0) = linspace(0, steps * dt, steps + 1); // 使用linspace来创建一个等距序列，操作与R的(0:30)一样
 
     // Store the initial conditions
-    rowvec infe_cases = ComputeInfeCase(agent_status);
-    results.row(0).subvec(1, 8) = infe_cases;
+    vec infe_cases = ComputeInfeCase(agent_status);
+    results.row(0).subvec(1, 8) = infe_cases.t();
 
-    /*
-    下面这一段是使用Rcpp标准库完成，可能效率低，但是不会出错
-        NumericVector infe_cases(8);
-        int nrows = agent_status.nrow();
-        // #pragma omp parallel for schedule(static)
-        for (int j = 0; j < 8; j++)
-        {
-            int count = 0;
-            for (int i = 0; i < nrows; i++)
-            {
-                if (agent_status(i, j) == 1)
-                {
-                    count++;
-                }
-            }
-            infe_cases[j] = count;
-        }
-        results.row(0).subvec(1, 8) = infe_cases.t(); // .t() is for transpose since vec is a column vector 这一行是使用Armadillo实现
+    size_t i, j;
 
-    */
-    size_t ts, i, j;
-
-    for (size_t ts = 0; ts < steps * dt; ts++)
+    for (size_t ts = 0; ts < steps; ts++) // time
     {
-        infe_cases = ComputeInfeCase(agent_status);
-        results.row(ts + 1).subvec(1, 8) = infe_cases;
+        infe_cases = ComputeInfeCase(agent_status); // Calculate the number of currently infected agents
+        results.row(ts + 1).subvec(1, 8) = infe_cases.t();
 
         int time = floor(results(ts, 0));
         float seasonal_force = (1 + beta_seasonal * cos((2 * M_PI * time / 52) - beta_phi));
+        vec beta = beta_value * seasonal_force;
 
-#pragma omp parallel for private(i, j)
-        for (size_t i = 0; i < N; i++)
+#pragma omp parallel for private(i, j) num_threads(ncores)
+        for (size_t i = 0; i < N; i++) // row
         {
-            for (size_t j = 0; j < 8; j++)
+            for (size_t j = 0; j < 8; j++) // column
             {
                 switch ((int)agent_status(i, j))
                 {
                 case 0:
                 {
-                    float beta = beta_value(j) * seasonal_force;
                     // pow是元素级别的求指数，与R中的(comp_j / comp_k)^inf_states功能一致，prod是连乘，与R一致
                     vec inf_states = conv_to<vec>::from(agent_status.row(i) == 1);
                     float delta = prod(pow((comp_value(j) / comp_value), inf_states));
-                    float lambda = beta * delta * infe_cases(j) / N;
+                    float lambda = beta(j) * delta * infe_cases(j) / N;
                     State_probability(i, j) = 1 - exp(-lambda * dt);
                 }
                 break;
@@ -232,40 +98,30 @@ mat ModelSimCpp(List Parm)
             }
         }
 
-        // State_probability = ComputeStateTrans(time, dt, beta_seasonal, beta_phi, N, gamma, agent_status,
-        //                                      infe_cases, beta_value, comp_value, omega_value);
-
+        // Calculate state transition matrix based on random number (if State_probability bigger than random number, the agent should transition the state)
+        //  arma_rng::set_seed(seeds); //ChatGPT建议这里加上随机数设置，但是似乎在R中使用set.seed()就可以控制
         umat Comparing_matrix = (State_probability > randu<mat>(State_probability.n_rows, State_probability.n_cols));
 
-        /*
-            mat agent_status2 = agent_status;
-            //在mat中使用等号复制一个矩阵会产生一个新的副本，不会对原始的agent_status产生影响
-            //似乎这里不需要复制一个agent_status2了，因为下面的三个condition已经将要修改的位置限定死了
-        */
-
-        // State transition
+        // Locate the state that needs to be transformed
         umat condition_S = (agent_status == 0) % Comparing_matrix;
         umat condition_I = (agent_status == 1) % Comparing_matrix;
         umat condition_R = (agent_status == -1) % Comparing_matrix;
-
+        // State transition
         agent_status.elem(find(condition_S)).fill(1);
         agent_status.elem(find(condition_I)).fill(-1);
         agent_status.elem(find(condition_R)).fill(0);
 
-        // agent_status = stateTransition(State_probability, agent_status);
-
-        int num_cols = agent_status.n_cols;
-
-        for (size_t col = 0; col < num_cols; col++)
+        // Add new cases per dt
+        for (size_t col = 0; col < 8; col++)
         {
-            uvec zero_indicater = find(agent_status.col(col) == 0); // zero_indicater是一个标记了哪里是0的向量，如agent_status中第1，3，5个为0，那么zero_indicater则返回1，3，5
+            uvec S_Locate = find(agent_status.col(col) == 0); // S_Locate是一个标记了哪里是0的向量，如agent_status中第1，3，5个为0，那么S_Locate则返回1，3，5
 
-            if (zero_indicater.n_elem >= added_cases)
+            if (S_Locate.n_elem >= added_cases)
             {
-                zero_indicater = shuffle(zero_indicater); // 这里将zero_indicater的顺序打乱，这样在下一步找到10个填入病例的时候才能实现随机
+                S_Locate = shuffle(S_Locate); // 这里将S_Locate的顺序打乱，这样在下一步找到10个填入病例的时候才能实现随机
                 for (size_t k = 0; k < added_cases; k++)
                 {
-                    agent_status(zero_indicater(k), col) = 1;
+                    agent_status(S_Locate(k), col) = 1;
                 }
             }
         }
